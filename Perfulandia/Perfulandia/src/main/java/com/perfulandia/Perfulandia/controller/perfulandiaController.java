6bce701b7f6c1290c5ab59fbdcdffc0db514159f
Jimena <ji.galicia@duocuc.cl>
2025-05-07 22:33:34 -0400

    Add files via upload

---
+++ Perfulandia/Perfulandia/src/main/java/com/perfulandia/Perfulandia/controller/perfulandiaController.java
@@ -0,0 +1,11 @@
+package com.perfulandia.Perfulandia.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class perfulandiaController {
+    @GetMapping("/perfulandia")
+    public String perfulandia() {
+        return "perfulandia";
+    }
+}
